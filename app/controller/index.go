83a4118ba33b6f817ada193d75f77d63d6900a99
gatopardo <gato.pardo@gmail.com>
2021-04-24 17:41:01 -0400

    search error 23

--- app/controller/index.go
+++ app/controller/index.go
@@ -12,12 +12,12 @@ func IndexGET(w http.ResponseWriter, r *http.Request) {
 	// Get session
 	session := model.Instance(r)
 
-	if session.Values["id"] != nil {
-		// Display the view
-		v := view.New(r)
-		v.Name = "index/auth"
-		v.Render(w)
-	} else {
+//	if session.Values["id"] != nil {
+//		// Display the view
+//		v := view.New(r)
+//		v.Name = "index/auth"
+//		v.Render(w)
+//	} else {
 		// Display the view
 		v := view.New(r)
 		v.Name = "index/anon"
